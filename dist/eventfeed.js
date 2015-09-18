82d76ea3e6cf37b5f87fd55413f568d63f369517
sethmcleod <me@sethmcleod.com>
2015-09-18 16:03:40 -0400

    Unique Key Generater

--- dist/eventfeed.js
+++ dist/eventfeed.js
@@ -141,6 +141,13 @@
       data = data || 'empty';
       console.log('Parsing data...');
       console.log('no data to parse.');
+    // helper function to generate unique key
+    function _genKey() {
+      var S4;
+      S4 = function() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      };
+      return "" + (S4()) + (S4()) + (S4()) + (S4());
     };
 
     // helper function for parsing month
